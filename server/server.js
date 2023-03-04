fb08b7b66ff1f6bc66deef746ec0c79c038a48a4
Faris Alsalih <falsalih@aol.de>
2023-03-04 21:33:07 +0100

    del redun. code, move  listen method to last line

--- server/server.js
+++ server/server.js
@@ -8,30 +8,22 @@ const app = express();
 
 app.use(compression());
 
-app.listen(port, () => {console.log("Listening on port " + port)});
-
 app.use("/app_static", express.static(path.join(__dirname, '../suche/app_static')));
+
 if(process.env.NODE_ENV === "production"){
   proxy_port = 6060;
 }else {
   proxy_port = 8080;
-
 }
 app.use('/api', proxy({target: `http://localhost:${proxy_port}`, secure: false}));
 app.use("/public", express.static(path.join(__dirname, '../api/public'), {fallthrough: false}));
-// console.log:
-// console.log('path to public folder from server :',path.join(__dirname, '../api/public'))
-// console.log("target " , `http://localhost:${proxy_port}`)
-
-// redundant legacy code
-// since we are doing the corsConfig on the backend server "zuugle-api" so we do not need to set cors headers here
-// app.use((req, res, next) => {
-//   res.append('Access-Control-Allow-Origin', ['*']);
-//   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.append('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// });
 
 app.get([ '/*' ], function(req, res) {
   res.sendFile(path.join(__dirname, '../suche/index.html'));
 });
+
+app.listen(port, () => {console.log("Listening on port " + port)});
+
+ 
+
+
