28f4f71654ae9cf23036edb5224e920a53fff51f
Faris Alsalih <falsalih@aol.de>
2023-03-16 10:29:08 +0100

    comment out all code in file

--- server/server.js
+++ server/server.js
@@ -1,28 +1,28 @@
-var express = require('express'),
-    path = require('path'),
-    compression = require('compression'),
-    proxy = require('http-proxy-middleware');
+// var express = require('express'),
+//     path = require('path'),
+//     compression = require('compression'),
+//     proxy = require('http-proxy-middleware');
 
-var port = 4000;
-const app = express();
+// var port = 4000;
+// const app = express();
 
-app.use(compression());
+// app.use(compression());
 
-app.use("/app_static", express.static(path.join(__dirname, '../suche/app_static')));
+// app.use("/app_static", express.static(path.join(__dirname, '../suche/app_static')));
 
-if(process.env.NODE_ENV === "production"){
-  proxy_port = 6060;
-}else {
-  proxy_port = 8080;
-}
-app.use('/api', proxy({target: `http://localhost:${proxy_port}`, secure: false}));
-app.use("/public", express.static(path.join(__dirname, '../api/public'), {fallthrough: false}));
+// if(process.env.NODE_ENV === "production"){
+//   proxy_port = 6060;
+// }else {
+//   proxy_port = 8080;
+// }
+// app.use('/api', proxy({target: `http://localhost:${proxy_port}`, secure: false}));
+// app.use("/public", express.static(path.join(__dirname, '../api/public'), {fallthrough: false}));
 
-app.get([ '/*' ], function(req, res) {
-  res.sendFile(path.join(__dirname, '../suche/index.html'));
-});
+// app.get([ '/*' ], function(req, res) {
+//   res.sendFile(path.join(__dirname, '../suche/index.html'));
+// });
 
-app.listen(port, () => {console.log("Listening on port " + port)});
+// app.listen(port, () => {console.log("Listening on port " + port)});
 
  
 
