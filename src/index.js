b2f89e9156575b9b6c5c6d8281457138685174c9
Faris Alsalih <falsalih@aol.de>
2023-04-05 12:57:39 +0200

    do React18 adjustments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
+// import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from "./rootReducer";
 import { Helmet } from 'react-helmet';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(rootReducer, composeEnhancers(
@@ -27,14 +28,23 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
     document.head.appendChild(msViewportStyle);
 }
 
-ReactDOM.render((
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+//   <React.StrictMode>
     <Provider store={store}>
-        <div>
-            <Helmet>
-                <link rel="canonical" href="https://www.zuugle.at"/>
-                <link id="favicon" rel="icon" href="/app_static/favicon.png" type="image/x-icon"/>
-            </Helmet>
-        </div>
-        <App />
+      <div>
+        <Helmet>
+          <link rel="canonical" href="https://www.zuugle.at" />
+          <link
+            id="favicon"
+            rel="icon"
+            href="/app_static/favicon.png"
+            type="image/x-icon"
+          />
+        </Helmet>
+      </div>
+      <App />
     </Provider>
-), document.getElementById('root'));
+//   </React.StrictMode>
+);
