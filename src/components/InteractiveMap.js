6c79f1c008a21367194d199c39b8b98f800910fd
Faris Alsalih <falsalih@aol.de>
2023-04-21 22:06:35 +0200

    uEffect :initial. map then remove , test alt scen

--- src/components/InteractiveMap.js
+++ src/components/InteractiveMap.js
@@ -1,92 +1,267 @@
 import * as React from 'react';
-import {MapContainer, TileLayer, Marker, Polyline} from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-// import "react-leaflet-fullscreen/dist/styles.css";
-// import { FullscreenControl } from "react-leaflet-fullscreen";
-import {useEffect, useRef} from "react";
-import L from 'leaflet';
+import {useRef, useEffect, useMemo} from 'react';
+import { MapContainer, TileLayer, Marker, Polyline, useMap } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L, { divIcon } from 'leaflet';
 
-// description:
-//the component "InteractiveMap" renders an interactive map using the React Leaflet library. The map displays markers and polylines based on the input parameters passed to the component. The purpose of the component is to display a map with different markers and polylines that represent different routes or paths. The component uses Leaflet icons for the start and end markers and a tile layer from opentopo.bahnzumberg.at. 
-//Tile layers:  provided by the OpenTopoMap project, which is a free and open-source topographic map that is based on OpenStreetMap data. The map features contour lines, hillshading, and a terrain background layer that provides information about the landscape. The opentopo.bahnzumberg.at URL is one of several servers hosting this map.
-//detailed description: below final lines
-export default function InteractiveMap({gpxPositions, anreiseGpxPositions, abreiseGpxPositions}){
-    const mapRef = useRef();
+export default function InteractiveMap({ gpxPositions, anreiseGpxPositions, abreiseGpxPositions }) {
     const polyRef = useRef();
+    const mapRef  = useRef(null);
 
-    let StartIcon = L.icon({
-        iconUrl: 'app_static/img/pin-icon-start.png',
-        shadowUrl: 'app_static/img/pin-shadow.png',
-        iconSize: [30, 41],
-        iconAnchor: [15, 41],
-    });
+  const startIcon = L.icon({
+    iconUrl: 'app_static/img/pin-icon-start.png',
+    shadowUrl: 'app_static/img/pin-shadow.png',
+    iconSize: [30, 41],
+    iconAnchor: [15, 41],
+  });
+  const endIcon = L.icon({
+    iconUrl: 'app_static/img/pin-icon-end.png',
+    shadowUrl: 'app_static/img/pin-shadow.png',
+    iconSize: [30, 41],
+    iconAnchor: [15, 41],
+  });
 
-    let EndIcon = L.icon({
-        iconUrl: 'app_static/img/pin-icon-end.png',
-        shadowUrl: 'app_static/img/pin-shadow.png',
-        iconSize: [30, 41],
-        iconAnchor: [15, 41],
-    });
+  const StartMarker = ({ position }) => {
+    return <Marker position={position} icon={startIcon}></Marker>;
+  };
 
-    useEffect(() => {
-        if(!!polyRef.current){
-            if(!!mapRef && !!mapRef.current){
-                mapRef.current.fitBounds(polyRef.current.getBounds());
-            }
-        }
-    })
+  const EndMarker = ({ position }) => {
+    return <Marker position={position} icon={endIcon}></Marker>;
+  };
 
-    const getStartMarker = () => {
-        if(!!anreiseGpxPositions && anreiseGpxPositions.length > 0){
-            return <Marker position={anreiseGpxPositions[0]} icon={StartIcon}></Marker>;
-        } else if(!!gpxPositions && gpxPositions.length > 0){
-            return <Marker position={gpxPositions[0]} icon={StartIcon}></Marker>;
-        }
+  // const MapFullScreenControl = () => {
+  //   const map = useMap();
+  //   return (
+  //     <div className="leaflet-control-container">
+  //       <div className="leaflet-top leaflet-right">
+  //         <div className="leaflet-control-zoom leaflet-bar leaflet-control">
+  //           <a
+  //             className="leaflet-control-zoom-in fullscreen-toggle-icon leaflet-bar-part leaflet-bar-part-top-and-bottom"
+  //             href="#"
+  //             title="Toggle fullscreen"
+  //             role="button"
+  //             onClick={() => {
+  //               map.toggleFullscreen();
+  //             }}
+  //           >
+  //             <span className="fa fa-expand" />
+  //           </a>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
+  // };
+
+  // useEffect(() => {
+  //   console.log("L 57: inside first useEffect ")
+  //   const map = mapRef.current;
+
+  //   // remove previous layers and markers
+  //   map.eachLayer(layer => {
+  //     if (!layer._url) { // ignore tile layers
+  //       map.removeLayer(layer);
+  //     }
+  //   });
+
+  //   if (!!polyRef.current){
+  //     map.fitBounds(polyRef.current.getBounds());
+  //   }
+
+  //   return () => {
+  //     // remove the map when the component is unmounted
+  //     map.remove();
+  //   }
+  // }, [gpxPositions, anreiseGpxPositions, abreiseGpxPositions]);
+
+  // useEffect(() => { // do we need to initialize before use?
+  //   if(!!mapRef && mapRef.current != null) {
+  //     mapRef.current=null;
+  //     console.log("L 59 : mapRef is initialized")
+  //   }
+  // })
+
+  // useEffect(() => {
+  //   const map = mapRef.current?.leafletElement;
+
+  //   function handleClick() {
+  //     console.log('Map clicked!');
+  //   }
+
+  //   if (map) {
+  //     map.on('click', handleClick);
+  //   }
+
+  //   return () => {
+  //     if (map) {
+  //       map.off('click', handleClick);
+  //     }
+  //   };
+  // }, []);
+
+useEffect(() => {
+  // function initializingMap() {
+  // call this method before you initialize your map.
+  // var container = L.DomUtil.get("map");
+  //   let container= undefined ;
+
+  //   if(!!mapRef && !!mapRef.current._container){
+  //     container = mapRef.current._container;
+  //   }
+  //   if (container != null) {
+  //     container._leaflet_id = null;
+  //   }
+  //   console.log("Iniside initializeMap , container._leaflet_id ", container._leaflet_id)
+  // }
+  const initializeMap = () => {
+    if (!!mapRef && !!mapRef.current) {
+      // console.log(
+      //   " initializeMap : before if , _leaflet_id :",
+      //   mapRef.current._leaflet_id
+      // );
+      if (!!mapRef.current._leaflet_id && mapRef.current._leaflet_id != (null || undefined)) {
+        console.log("Just before initialization")
+        mapRef.current._leaflet_id = null;
+      }
     }
+  };
+  initializeMap();
+  //clg
+  // mapRef.current
+  //   ? console.log(
+  //       "L130 Interactive, ouside if(), mapRef.current._leaflet_id is :",
+  //       mapRef.current._leaflet_id
+  //     )
+  //   : console.log("L133 mapRef.current is falsy");
 
-    const getEndMarker = () => {
-        if(!!abreiseGpxPositions && abreiseGpxPositions.length > 0){
-            return <Marker position={abreiseGpxPositions[abreiseGpxPositions.length -1]} icon={EndIcon}></Marker>;
-        } else if(!!gpxPositions && gpxPositions.length > 0){
-            return <Marker position={gpxPositions[gpxPositions.length - 1]} icon={EndIcon}></Marker>;
-        }
+  return () => {
+    // remove the map when the component is unmounted
+    let map = mapRef.current;
+    // map ? console.log("L138 returning InteractiveM , map is", map) : console.log("L138 Interactive, this is", map);
+    if(!!map) {
+      map = map.remove();
+    } 
+  };
+}, []);
+
+useEffect(() => {
+  let map = mapRef.current;
+    // (map && map._leaflet_id) ? console.log("147  InteractiveM useEffect2 , map._leaflet_id is", map._leaflet_id) : console.log("L147 Interactive, this is", map);
+  if (!!mapRef && !!mapRef.current) {
+    if (!!polyRef.current) {
+      mapRef.current.fitBounds(polyRef.current.getBounds());
     }
+  }
+});
 
-    return <MapContainer
-        scrollWheelZoom={true}
-        maxZoom={17}
-        center={[47.800499,13.044410]} zoom={13} style={{ height: "100%", width: "100%" }} whenCreated={(mapInstance)=> { mapRef.current = mapInstance }}>
-        <TileLayer
-            url="https://opentopo.bahnzumberg.at/{z}/{x}/{y}.png"
-        />
-        {(!!gpxPositions && gpxPositions.length > 0) && [<Polyline
-            ref={polyRef}
-            pathOptions={{ fillColor: 'red', color: 'red' }}
-            positions={gpxPositions}
-        />]}
-        {(!!anreiseGpxPositions && anreiseGpxPositions.length > 0) && [
+// useEffect(() => {
+//     if(!!polyRef.current){
+//         if(!!mapRef && !!mapRef.current){
+//             mapRef.current.fitBounds(polyRef.current.getBounds());
+//         }
+//     }
+// })
+
+// useEffect(() => {
+//     if(polyRef.current && mapRef.current) {
+//         mapRef.current.fitBounds(polyRef.current.getBounds());
+//     }
+// }, [polyRef.current]);
+
+  const getStartMarker = () => {
+    if (anreiseGpxPositions && anreiseGpxPositions.length > 0) {
+      return <StartMarker position={anreiseGpxPositions[0]} />;
+    } else if (gpxPositions && gpxPositions.length > 0) {
+      return <StartMarker position={gpxPositions[0]} />;
+    }
+  }
+  const getEndMarker = () => {
+    if (!!abreiseGpxPositions && abreiseGpxPositions.length > 0) {
+      return <EndMarker position={abreiseGpxPositions[abreiseGpxPositions.length - 1]} />;
+    } else if (!!gpxPositions && gpxPositions.length > 0) {
+      return <EndMarker position={gpxPositions[gpxPositions.length - 1]} />;
+    }
+  };
+
+  // const memoizedMapContainer = React.useCallback( () => {
+  //   return (
+  //     <MapContainer
+  //     // key={()=>generateKey()}
+  //     ref={mapRef}
+  //     scrollWheelZoom={true}
+  //     maxZoom={17}
+  //     center={[47.800499, 13.04441]}
+  //     zoom={13}
+  //     style={{ height: "100%", width: "100%" }}
+  //     // whenCreated={(mapInstance)=> { mapRef.current = mapInstance }}
+  //   >
+  //       <TileLayer url="https://opentopo.bahnzumberg.at/{z}/{x}/{y}.png" />
+  //       {!!gpxPositions && gpxPositions.length > 0 && (
+  //           <Polyline
+  //           ref={polyRef}
+  //           pathOptions={{ color: 'red' }}
+  //           positions={gpxPositions}
+  //           />
+  //       )}
+  //       {getStartMarker()}
+  //       {getEndMarker()}
+  //       {!!anreiseGpxPositions && anreiseGpxPositions.length > 0 && (
+  //       <Polyline
+  //           pathOptions={{ color: 'blue' }}
+  //           positions={anreiseGpxPositions}
+  //       />
+  //       )}
+  //       {!!abreiseGpxPositions && abreiseGpxPositions.length > 0 && (
+  //       <Polyline
+  //           pathOptions={{ color: 'green' }}
+  //           positions={abreiseGpxPositions}
+  //       />
+  //       )}
+  //       {/* <MapFullScreenControl /> */}
+
+  //   </MapContainer>
+  
+  //     )
+  //   },[gpxPositions])
+
+
+  return (
+    // <div>
+    //   {memoizedMapContainer()}
+    // </div>
+    <MapContainer
+      // key={()=>generateKey()}
+      ref={mapRef}
+      scrollWheelZoom={true}
+      maxZoom={17}
+      center={[47.800499, 13.04441]}
+      zoom={13}
+      style={{ height: "100%", width: "100%" }}
+      // whenCreated={(mapInstance)=> { mapRef.current = mapInstance }}
+    >
+        <TileLayer url="https://opentopo.bahnzumberg.at/{z}/{x}/{y}.png" />
+        {!!gpxPositions && gpxPositions.length > 0 && (
             <Polyline
-                pathOptions={{ fillColor: 'blue', color: 'blue' }}
-                positions={anreiseGpxPositions}
+            ref={polyRef}
+            pathOptions={{ color: 'red' }}
+            positions={gpxPositions}
             />
-        ]}
-        {(!!abreiseGpxPositions && abreiseGpxPositions.length > 0) && <Polyline
-            pathOptions={{ fillColor: 'blue', color: 'blue' }}
+        )}
+        {getStartMarker()}
+        {getEndMarker()}
+        {!!anreiseGpxPositions && anreiseGpxPositions.length > 0 && (
+        <Polyline
+            pathOptions={{ color: 'blue' }}
+            positions={anreiseGpxPositions}
+        />
+        )}
+        {!!abreiseGpxPositions && abreiseGpxPositions.length > 0 && (
+        <Polyline
+            pathOptions={{ color: 'green' }}
             positions={abreiseGpxPositions}
-        />}
-        {
-            getStartMarker()
-        }
-        {
-            getEndMarker()
-        }
+        />
+        )}
+        {/* <MapFullScreenControl /> */}
 
-        {/* <FullscreenControl /> */}
     </MapContainer>
+  )
 }
-
-//detailed dscription:
-//The InteractiveMap component renders an interactive map using the React Leaflet library. The map displays markers and polylines based on the input parameters passed to the component. The purpose of the component is to display a map with different markers and polylines that represent different routes or paths. The component uses Leaflet icons for the start and end markers and a tile layer from OpenTopoMap project, which is a free and open-source topographic map based on OpenStreetMap data. The map features contour lines, hillshading, and a terrain background layer that provides information about the landscape.
-// The component also includes a FullscreenControl from the react-leaflet-fullscreen library to enable the user to view the map in fullscreen mode. The component uses the useEffect hook to adjust the zoom level of the map to fit the bounds of the polyline. The component uses ref to get access to the Leaflet map instance and the polyline instance.
-// The InteractiveMap component takes in three parameters, gpxPositions, anreiseGpxPositions, and abreiseGpxPositions, which are arrays of coordinates that define the routes or paths to display on the map. The component conditionally renders the start and end markers based on the input parameters. If anreiseGpxPositions or abreiseGpxPositions are passed, it uses the first or last coordinate of the array, respectively, for the start or end marker. Otherwise, if only gpxPositions is passed, it uses the first or last coordinate of that array for the start or end marker.
-// The InteractiveMap component also renders a TileLayer component that provides the tile layer for the map, and Polyline components to render the routes or paths on the map.
