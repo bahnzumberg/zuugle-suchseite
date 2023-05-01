9651b35f76c2e1d3c3793313b43936fef4e63cdd
Tobias Haindl <t.haindl@gmx.net>
2023-05-01 20:50:30 +0200

    #4 Add documentation

--- src/hooks/useMatomo.js
+++ src/hooks/useMatomo.js
@@ -2,6 +2,12 @@ import * as React from 'react';
 import {useLocation} from 'react-router-dom';
 import MatomoTracker from '@jonkoops/matomo-tracker'
 
+/**
+ * A simple hook wrapping the MatomoTracker class provided by @jonkoops/matomo-tracker.
+ * Unfortunately, this package is no longer actively maintained but I believe it is not very likely that the Matomo API will change a lot in the future.
+ * If it is necessary to remove the package only this hook should be affected and there should be no need to change the rest of the codebase.
+ */
+
 export function useMatomo({
                               enableAutoPageTrack,
                               enableLinkTracking,
