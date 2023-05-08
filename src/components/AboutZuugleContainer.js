db3b69ee8505589bddd4c206be8f75d12d044abe
FALSAL <coconto.info@gmail.com>
2023-05-08 08:37:38 +0300

    add i18next code for translation, use Trans

--- src/components/AboutZuugleContainer.js
+++ src/components/AboutZuugleContainer.js
@@ -4,10 +4,13 @@ import {Paper} from "@mui/material";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
+import { useTranslation, Trans } from 'react-i18next';
 
 //description
 //The AboutZuugleContainer.js file is a React component that exports a function component named RegionCard. This component returns a Paper component from Material UI with a dark background color and rounded corners. Inside the Paper, there is a Box component that contains a Grid component. The Grid has two columns, one for an image and the other for some text content. The image is sourced from a placeholder image and has rounded corners. The text content includes a heading, a subheading, a list of bullet points, and a button. The button redirects the user to the About page of the website. The component is intended to be used as a card that provides information about the Zuugle service to users.
 export default function RegionCard({}){
+
+    const{t, i18n} = useTranslation();
 
     return  <Paper sx={{
         background: '#000000',
@@ -20,27 +23,49 @@ export default function RegionCard({}){
                </Grid>
                <Grid item sm={6}>
                    <Box sx={{textAlign: 'left'}}>
-                       <Typography variant={"link"}>Über Zuugle</Typography>
+                        <Trans i18nKey='start.ueber_zuugle'>
+                            <Typography variant={"link"}>Über Zuugle</Typography>
+                        </Trans>
                    </Box>
-
-                    <Typography variant={"h3"} sx={{textAlign: 'left', marginTop: '10px'}}>Entdecke vielseitige Bergtouren im deutschsprachigen Alpenraum per Zug & Bus</Typography>
+                        <Trans i18nKey='start.entdecke_bergtouren'>
+                            <Typography variant={"h3"} sx={{textAlign: 'left', marginTop: '10px'}}>
+                                Entdecke vielseitige Bergtouren im deutschsprachigen Alpenraum per Zug & Bus
+                            </Typography>
+                        </Trans>
 
                    <ul className={"about-bullets"}>
                        <li>
-                           <Typography variant={"text"}>Öffentliche Erreichbarkeit garantiert</Typography><br/>
+                            <Trans i18nKey="start.oeffentliche_erreichbarkeit">
+                                <Typography variant={"text"}>Öffentliche Erreichbarkeit garantiert</Typography><br/>
+                            </Trans>
                        </li>
                        <li>
-                           <Typography variant={"text"}>Hin- & Rückreise </Typography><br/>
+                            <Trans i18nKey="start.hin_und_rueckreise">
+                                <Typography variant={"text"}>Hin- & Rückreise </Typography><br/>
+                            </Trans>
                        </li>
                        <li>
-                           <Typography variant={"text"}>Anerkannte qualitativ hochwertige Touren</Typography><br/>
+                            <Trans i18nKey='start.anerkannte_hochwertige_touren' >
+                                <Typography  variant={"text"}>
+                                    Anerkannte qualitativ hochwertige Touren
+                                </Typography><br/>
+                            </Trans>
                        </li>
                        <li>
-                           <Typography variant={"text"}>Eintages & Mehrtagestouren</Typography><br/>
+                            <Trans i18nKey='start.ein_und_mehrtagestouren'>
+                                <Typography variant={"text"}>
+                                    Eintages & Mehrtagestouren
+                                </Typography><br/>
+                            </Trans>
+
                        </li>
                    </ul>
                    <Box sx={{textAlign: 'left', marginTop: '20px'}}>
-                       <Button variant={"contained"} className={"button-more"} onClick={() => window.open(`${window.location.protocol}//${window.location.host}/about`)}>Mehr erfahren</Button>
+                        <Trans i18nKey='start.mehr_erfahren'>
+                            <Button variant={"contained"} className={"button-more"} onClick={() => window.open(`${window.location.protocol}//${window.location.host}/about`)}> 
+                                Mehr erfahren
+                            </Button>
+                        </Trans>
                    </Box>
                </Grid>
            </Grid>
