119ca66cdbebc90f1e80adcfa0e0f5c40515506d
FALSAL <coconto.info@gmail.com>
2023-05-10 09:06:04 +0300

    add code for Impressum and About, improve privacy

--- src/translations/translation.labels.js
+++ src/translations/translation.labels.js
@@ -7,9 +7,8 @@ const labels_privacy = {
   keine_personenbezogenen_daten: '...',
   analysedienst: '...',
   welcher_dienst: '...',
-  matomo_1: 'Wir setzen die Open Source Software {{matomo}} in der selbst-gehosteten Variante ein. Die Daten befinden sich daher auf einem Server in Deutschland in der EU.',
-//   matomo_1: '...',
-//   matomo_2: '...',
+  matomo_1: '...',
+  matomo_2: '...',
   welche_daten: '...',
   abgespeichert_werden: '...',
   cookies: '...',
@@ -25,10 +24,65 @@ const labels_privacy = {
 
 export const getPrivacyLabels = () => {
   const {t} = useTranslation();
+ let labels = [];
 
   Object.keys(labels_privacy).forEach(key => {
-    labels_privacy[key] = t(`privacy.${key}`)
+    labels[key] = t(`privacy.${key}`)
   })
 
-  return labels_privacy;
+  return labels;
+}
+
+const labels_impressum = {
+  verantwortlich_fuer_inhalt: '...',
+  spendenkonto: '...',
+  kontakt: '...',
+  vereinsvorstand: '...',
+  ziel_und_ausrichtung: '...',
+  non_profit_org1: '...',
+  und:'...',
+  betreiben:'...',
+  non_profit_org2: '...',
+  haftung_inhalte: '...',
+  beschreibungen_fotos: '...' ,
+  haftung_fuer_links: '...',
+  verknuepfung_links: '...',
+  lizenzbedingungen: '...',
+  anfuehrung_quellen: '...',
+  hier: '...',
+  angeführt: '...',
+}
+
+export const getImpressumLabels = () => {
+  const {t} = useTranslation();
+  let labels = [];
+
+  Object.keys(labels_impressum).forEach(key => {
+    labels[key] = t(`impressum.${key}`)
+  })
+
+  return labels;
+}
+
+const labels_about = {
+  was_ist_zuugle: '...',
+  zuugle_erklaerung_1: '...',
+  zuugle_erklaerung_2: '...',
+  zuugle_erklaerung_3: '...',
+  wie_und_wofuer: '...',
+  entwicklung_von_zuugle_1: '...',
+  entwicklung_von_zuugle_2:'...',
+  entwicklung_von_zuugle_3:'...',
+  entwicklung_von_zuugle_4:'...',
+  entwicklung_von_zuugle_5:'...',
+}
+
+export const getAboutLabels =()=> {
+  const{t} =useTranslation();
+
+  Object.keys(labels_about).forEach(key => {
+    labels_about[key] = t(`about.${key} `)
+  })
+
+  return labels_about;
 }
