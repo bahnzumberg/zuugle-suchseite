71c434681f630416f5d0fa16668b298c23e98dd3
Thomas Prikryl <thomas.prikryl@bearingpoint.com>
2023-05-11 15:31:08 +0200

    [TOUR_DETAIL] Property draft

--- src/views/Main/DetailReworked.js
+++ src/views/Main/DetailReworked.js
@@ -16,13 +16,10 @@ import {
 import {compose} from "redux";
 import {connect} from "react-redux";
 import {loadGPX} from "../../actions/fileActions";
-import GpxParser from "gpxparser";
-import moment from "moment/moment";
-import {Buffer} from "buffer";
-import fileDownload from "js-file-download";
-import {parseFileName} from "../../utils/globals";
-import Button from "@mui/material/Button";
-import CircularProgress from "@mui/material/CircularProgress";
+import gpxParser from "gpxparser";
+import {Chip} from "@mui/material";
+import TourDetailProperties from "../../components/TourDetailProperties";
+
 
 const setGpxTrack = (url, loadGPX, _function) => {
     loadGPX(url).then(res => {
@@ -86,129 +83,41 @@ const DetailReworked = ({
     }, []);
 
     useEffect(() => {
+        console.log('HALLO');
         if (tour) {
             setGpxTrack(tour.gpx_file, loadGPX, setGpxPositions);
             setGpxTrack(tour.totour_gpx_file, loadGPX, setAnreiseGpxPositions);
             setGpxTrack(tour.fromtour_gpx_file, loadGPX, setAbreiseGpxPositions);
         }
-    }, [!!tour]);
+    }, [!!tour]); // Nicki ?
 
-    async function onDownload() {
-        try {
-            const response = await loadTourPdf({
-                id: tour?.id,
-                connection_id: !!activeConnection ? activeConnection.id : undefined,
-                connection_return_id: !!activeReturnConnection ? activeReturnConnection.id : undefined,
-                connection_return_ids: (!!connections[0].returns ? connections[0].returns.map(e => e.id) : []),
-                date
-            }) // TODO change to currently selected index
-            // handle response here
-            if (response) {
-                let pdf = undefined;
-                if (!!response.data) {
-                    response.data = JSON.parse(response.data);
-                    if (!!response.data.pdf) {
-                        pdf = response.data.pdf
-                    }
-                    ;
-                } else if (!response.data || !response.data.pdf) {
-                    console.log("no response")
-                }
+    return <Box sx={{"background-color": "#FFFFFF"}}>
+        <Box sx={{background: "#4992FF"}}>
 
-                if (!!pdf) {
-                    const fileName = response.data.fileName ? response.data.fileName : "";
-                    const buf = Buffer.from(pdf, 'base64');
-                    fileDownload(buf, fileName, "application/pdf");
-                }
-            } else {
-                console.log("no response is returned")
-            }
-        } catch (error) {
-            console.log('error : ', error)
-        }
-    }
-
-
-    const onDownloadGpx = () => {
-        if (!!activeReturnConnection && activeReturnConnection.fromtour_track_key && !!activeConnection && !!activeConnection.totour_track_key) {
-            loadTourGpx({
-                id: tour.id,
-                key_anreise: activeConnection.totour_track_key,
-                key_abreise: activeReturnConnection.fromtour_track_key,
-                type: "all"
-            }).then((res) => {
-                    if (!!res && !!res.data) {
-                        fileDownload(res.data, parseFileName(tour.title, "zuugle_", ".gpx"));
-                    }
-                },
-                (err) => {
-                    console.log('error: ', err)
-                });
-        } else {
-            loadTourGpx({id: tour.id}).then((res) => {
-                    if (!!res && !!res.data) {
-                        fileDownload(res.data, parseFileName(tour.title, "zuugle_", ".gpx"));
-                    }
-                },
-                (err) => {
-                    console.log('error: ', err)
-                });
-        }
-    }
-
-    const buttonsDisabled = () => {
-        return !!!tour || !!!tour.gpx_file || !!!activeConnection || !!!activeConnection.totour_track_key || !!!activeReturnConnection || !!!activeReturnConnection.fromtour_track_key;
-    }
-
-
-    return <>
         <SearchContainer goto={"/suche"}/>
-        {(window?.selectedTour) && <div>
-            <pre>{JSON.stringify(window?.selectedTour, null, 2)}</pre>
-        </div>}
-        <Box sx={{padding: 3}}>
-            <Box className="mt-3">
-                <Typography variant="h5">{tour?.range}</Typography>
-            </Box>
-            <Box className="mt-3">
-                <Typography variant="h4">{tour?.title}</Typography>
-            </Box>
         </Box>
-        <div>
-            <Box sx={{width: "100%", position: "relative", height: 300}}>
-                <InteractiveMap gpxPositions={gpxPositions} anreiseGpxPositions={anreiseGpxPositions}
-                                abreiseGpxPositions={abreiseGpxPositions}/>
+        <Box>
+            <Box sx={{padding: 3, "text-align": "left"}}>
+                <Box className="mt-3">
+                    <Typography variant="h4">{tour?.title}</Typography>
+                </Box>
+                <Box className="mt-3">
+                    <span className="tour-detail-range-tag" >{tour?.range}</span>
+                </Box>
             </Box>
-        </div>
-        <div>
-            <Box sx={{marginTop: '20px'}}>
-                <Button variant="outlined" fullWidth disabled={buttonsDisabled()} onClick={() => {
-                    onDownloadGpx();
-                }}>
-                    {!!isGpxLoading ? <CircularProgress sx={{width: "20px", height: "20px"}}
-                                                        size={"small"}/> : <>GPX Download</>}
-                </Button>
-                <Button sx={{height: "100%"}} variant="outlined" fullWidth disabled={buttonsDisabled()}
-                        onClick={onDownload}>
-                    {!!isPdfLoading ? <CircularProgress sx={{width: "20px", height: "20px"}} size={"small"}/> : 'PDF'}
-                </Button>
-                {!!buttonsDisabled() &&
-                    <div style={{marginTop: "10px"}}>
-                        <span style={{fontSize: "12px", color: "#101010", lineHeight: "12px"}}>Ein Download ist nur möglich wenn eine Verbindung gefunden wurde. Versuchen Sie bitte
-                            {/*<span style={{lineHeight: "12px", color: "#4992FF"}} className={"cursor-link"}*/}
-                            {/*      onClick={() => handleTabChange(null, 1)}> hier </span>*/}
-                            einen anderen Tag zu wählen.</span>
-                    </div>
-                }
-            </Box>
-        </div>
-        <div>
-            <pre>All connections: {JSON.stringify(connections?.length)}</pre>
-            <pre>Active connection: {JSON.stringify(activeConnection?.id)}</pre>
-            <pre>Active return connection: {JSON.stringify(activeReturnConnection?.id)}</pre>
-        </div>
+            <div>
+                <Box sx={{width: "100%", position: "relative", height: 300}}>
+                    <InteractiveMap gpxPositions={gpxPositions} anreiseGpxPositions={anreiseGpxPositions}
+                                    abreiseGpxPositions={abreiseGpxPositions}/>
+                </Box>
+                <TourDetailProperties tour={tour}></TourDetailProperties>
+            </div>
+            <div>
+
+            </div>
+        </Box>
         <Footer></Footer>
-    </>;
+    </Box>;
 }
 
 const mapDispatchToProps = {
