0749b6e0600b876fafd584c596796a77de503f84
FALSAL <coconto.info@gmail.com>
2023-05-23 12:53:28 +0200

    improve handleChange to set localStorage

--- src/components/Language/LanguageDropdown.js
+++ src/components/Language/LanguageDropdown.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import InputLabel from "@mui/material/InputLabel";
+// import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
@@ -19,10 +19,15 @@ export default function LanguageDropdown() {
   const {i18n} = useTranslation();
 
 
+  // const handleChange = (e) => {
+  //   i18n.changeLanguage(e.target.value);
+  // };
   const handleChange = (e) => {
-    i18n.changeLanguage(e.target.value);
+    const selectedLanguage = e.target.value;
+    i18n.changeLanguage(selectedLanguage);
+    localStorage.setItem('lang', selectedLanguage);
   };
-
+  
   return (
     <Box sx={{ minWidth: 120 }}>
       <FormControl fullWidth>
