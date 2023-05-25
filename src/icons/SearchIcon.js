8d3cec699dd88086e871ec672403a8d5595adbad
Richard Korn <richard.korn@bearingpoint.com>
2023-05-25 18:49:34 +0200

    [TOUR_DETAIL] SearchReworked - Rename to SearchIcon.js

--- src/icons/SearchIcon.js
+++ src/icons/SearchIcon.js
@@ -2,6 +2,6 @@ import * as React from "react"
 import { ReactComponent as YourSvg } from "./../icons/svg/ic_search.svg"
 import { styles } from "./defaults"
 
-export default function Search(props) {
+export default function SearchIcon(props) {
     return <YourSvg style={{ ...styles, ...props.style }} />
 }
