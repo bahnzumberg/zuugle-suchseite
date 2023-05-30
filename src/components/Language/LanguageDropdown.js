a4b089cc122baa629b3dd7e6b0dad151677464cf
FALSAL <coconto.info@gmail.com>
2023-05-30 19:33:49 +0200

    uncomment import mui input element

--- src/components/Language/LanguageDropdown.js
+++ src/components/Language/LanguageDropdown.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-// import InputLabel from "@mui/material/InputLabel";
+import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
