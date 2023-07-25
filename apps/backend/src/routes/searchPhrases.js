225a2d43c443137b504733d08cd13e74180bd6c4
lenaehm <ehmled19@htl-kaindorf.at>
2023-07-25 10:42:03 +0200

    [AUTOSUGGEST] - DAT and Review changes

--- apps/backend/src/routes/searchPhrases.js
+++ apps/backend/src/routes/searchPhrases.js
@@ -3,29 +3,27 @@ let router = express.Router();
 import knex from "../knex";
 router.get('/', (req, res) => listWrapper(req, res));
 
+//calls the database statement (createQuery) and sends the therefor needed props
+//it also waits for the result of the statement and returns the status of it
 const listWrapper = async (req, res) => {
     const search = req.query.search;
 
     if (search.length > 128) {
-        console.log("search length: ", search.length);
         return res.status(400).json({success: false, error: 'Bad Request - search term is too long (max. 128 characters)'});
     }
 
     const city = req.query.city;
     const language = req.query.language;
 
-    let item = await createQuery("phrase", "search_phrase", city, search, language);
+    const item = await createQuery("phrase", "search_phrase", city, search, language);
 
-    let result = {
-        item
-    }
+    const result = item;
 
-    return res.status(200).json({success: true, phrases: result});
+    return res.status(200).json({success: true, items: result});
 }
 
+//queries through the database table "logsearchphrase" and returns the phrases that start with the search phrase
 const createQuery = async (field, alias, city, search, language) => {
-    let result = [];
-
     let query = knex('logsearchphrase').select(`${field}`)
         .as(alias)
         .count('* as CNT')
@@ -46,12 +44,12 @@ const createQuery = async (field, alias, city, search, language) => {
         query = query.andWhereRaw(`LOWER(${field}) LIKE '${search.toLowerCase()}%'`)
     }
 
-    result = await query.groupBy(field)
+    const queryResult = await query.groupBy(field)
         .orderBy(`CNT`, `desc`)
         .orderBy(field, `asc`)
         .limit(5);
 
-    result = result.map(entry => {
+    const result = queryResult.map(entry => {
         return {
             suggestion: entry[field]
         }
