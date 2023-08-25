fc6721a52576700a92dd55221f8bd8028845df22
Martin Heppner <105971399+martinheppner@users.noreply.github.com>
2023-08-25 10:36:50 +0200

    Added logos for bergsteigen and oew providers

--- src/icons/ProviderLogo.js
+++ src/icons/ProviderLogo.js
@@ -6,6 +6,8 @@ import {ReactComponent as HoehenrauschLogo} from './svg/provider/logo_hoehenraus
 import {ReactComponent as WandernSteiermarkLogo} from './svg/provider/logo_steiermark.svg';
 import {ReactComponent as FallbackLogo} from './svg/provider/logo_fallback.svg';
 import {ReactComponent as MaPZSsiLogo} from './svg/provider/logo_mapzssi.svg';
+import {ReactComponent as BergsteigenLogo} from './svg/provider/logo_bergsteigen.svg';
+import {ReactComponent as OewLogo} from './svg/provider/logo_oew.svg';
 
 const provider = {
     BAHN_ZUM_BERG: 'bahnzumberg',
@@ -14,9 +16,12 @@ const provider = {
     BERGFEXAT: 'bergfexat',
     BERGFEXCH: 'bergfexch',
     BERGFEXIT: 'bergfexit',
+    BERGFEXSI: 'bergfexsi',
     HOEHENRAUSCH: 'hoehenrausch',
     WANDERN_STEIERMARK: 'wandernsteiermarkat',
-    MAPZSSI: 'mapzssi'
+    MAPZSSI: 'mapzssi',
+    BERGSTEIGEN: 'bergsteigencom',
+    OEW: 'oew'
 }
 
 const providerStyles = {width: "48px" , height: "48px"};
@@ -31,13 +36,18 @@ export default function ProviderLogo(props) {
         case provider.BERGFEXAT:
         case provider.BERGFEXCH:
         case provider.BERGFEXIT:
+        case provider.BERGFEXSI:
             return <BergfexLogo style={{...providerStyles, ...props.style}}/>
         case provider.HOEHENRAUSCH:
             return <HoehenrauschLogo style={{...providerStyles, ...props.style}}/>
         case provider.WANDERN_STEIERMARK:
             return <WandernSteiermarkLogo style={{...providerStyles, ...props.style}}/>
         case provider.MAPZSSI:
-                return <MaPZSsiLogo style={{...providerStyles, ...props.style}}/>
+            return <MaPZSsiLogo style={{...providerStyles, ...props.style}}/>
+        case provider.BERGSTEIGEN:
+            return <BergsteigenLogo style={{...providerStyles, ...props.style}}/>
+        case provider.OEW:
+            return <OewLogo style={{...providerStyles, ...props.style}}/>
         default:
             return <FallbackLogo style={{...providerStyles, ...props.style}}/>
     }
