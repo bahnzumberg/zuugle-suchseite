8dbc51731525a603bf125351f8ac64991b44c452
FALSAL <coconto.info@gmail.com>
2023-08-28 18:18:05 +0200

    delete redundant code

--- src/views/Start/SearchContainer.js
+++ src/views/Start/SearchContainer.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-// import Search from "../../components/Search/Search";
 import Search from "../../components/Search/Search";
 
 export default function SearchContainer({
