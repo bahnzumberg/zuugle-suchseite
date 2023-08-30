c7f17edc0a5149bdd27be2dc6b598c57c977bbce
FALSAL <coconto.info@gmail.com>
2023-08-30 19:53:28 +0200

    fix issueTourprofil Abstände über test@zuugle.at #102

--- src/components/TourDetailProperties.js
+++ src/components/TourDetailProperties.js
@@ -23,28 +23,28 @@ const TourDetailProperties = ({tour}) => {
     return <>
         {tour && <div className="tour-detail-properties">
             <div className="tour-detail-properties-el">
-                <Typography variant={"infoKey"}>{ t('main.sportart') }</Typography>
+                <Typography variant={"infoKey"}>{t('main.sportart')} {" "} </Typography>
                 <Typography variant={"h5alt"}>{tour && translateTourType(tour.type)}</Typography>
             </div>
             <div className="tour-detail-properties-el">
-                <Typography variant={"infoKey"}>{t('main.tourdauer')}</Typography>
+                <Typography variant={"infoKey"}>{t('main.tourdauer')} {" "}</Typography>
                 <Typography
                     variant={"h5alt"}>{(tour?.number_of_days > 1) ? (tour?.number_of_days + "days") : convertNumToTime(tour?.total_tour_duration, true)}</Typography>
             </div>
             <div className="tour-detail-properties-el">
-                <Typography variant={"infoKey"}>{ t('main.distanz') }</Typography>
+                <Typography variant={"infoKey"}>{t('main.distanz')} {" "}</Typography>
                 <Typography variant={"h5alt"}>{formatNumber(tour?.distance) + ' km'}</Typography>
             </div>
             <div className="tour-detail-properties-el">
-                <Typography variant={"infoKey"}> {t('main.maximale_hoehe')} </Typography>
+                <Typography variant={"infoKey"}> {t('main.maximale_hoehe')} {" "}</Typography>
                 <Typography variant={"h5alt"}>{tour?.max_ele ? formatNumber(tour.max_ele) + ' m' : "-"}</Typography>
             </div>
             <div className="tour-detail-properties-el">
-                <Typography variant={"infoKey"}>{t('main.aufstieg')}</Typography>
+                <Typography variant={"infoKey"}>{t('main.aufstieg')} {" "}</Typography>
                 <Typography variant={"h5alt"}>{formatNumber(tour?.ascent, ' hm')}</Typography>
             </div>
             <div className="tour-detail-properties-el">
-                <Typography variant={"infoKey"}>{t('main.abstieg')}</Typography>
+                <Typography variant={"infoKey"}>{t('main.abstieg')}{" "}</Typography>
                 <Typography variant={"h5alt"}>{formatNumber(tour?.descent, ' hm')}</Typography>
             </div>
         </div>}
