f7105efb19b3cc342e548f1102a2088805e791f8
Martin Heppner <105971399+martinheppner@users.noreply.github.com>
2023-09-06 15:13:28 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const Main = lazy(() => import("./views/Main/Main"));
 const About = lazy(() => import("./views/Pages/About"));
 const Impressum = lazy(() => import("./views/Pages/Impressum"));
 const Privacy = lazy(() => import("./views/Pages/Privacy"));
+import { tryLoadAndStartRecorder } from '@alwaysmeticulous/recorder-loader'
 
 function App() {
   // production matomo ID
@@ -25,6 +26,8 @@ function App() {
     location.hostname.indexOf("www2.") !== -1
   ) {
     siteId = 11;
+
+    tryLoadAndStartRecorder({ projectId: '0HjVPphxK3XDsQ4ka8QMwfxlMW204RtKu2bL92KO', });
   }
 
   useMatomo({
