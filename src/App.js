c68b5944e576e858aebafa8212040001629d30ae
Martin Heppner <105971399+martinheppner@users.noreply.github.com>
2023-09-13 00:38:48 +0200

    New testrun with Matomo Tag Manager

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import CircularProgress from "@mui/material/CircularProgress";
 import DetailReworked from "./views/Main/DetailReworked";
-// import { useMatomo } from "./hooks/useMatomo";
+import { useMatomo } from "./hooks/useMatomo";
 import Search from "./components/Search/Search";
 const Main = lazy(() => import("./views/Main/Main"));
 const About = lazy(() => import("./views/Pages/About"));
@@ -30,22 +30,21 @@ function App() {
     // tryLoadAndStartRecorder({ projectId: '0HjVPphxK3XDsQ4ka8QMwfxlMW204RtKu2bL92KO', });
   }
 
-  /*
-  useMatomo({
-    hostConfig: {
-      siteId: siteId,
-      url: "https://stats.bahnzumberg.at",
-    },
-    enableAutoPageTrack: true,
-  });
-  */
-
+  // useMatomo({
+  //  hostConfig: {
+  //    siteId: siteId,
+  //    url: "https://stats.bahnzumberg.at",
+  //  },
+  //  enableAutoPageTrack: true,
+  // });
+  
   React.useEffect(() => {
     var _mtm = window._mtm = window._mtm || [];
     _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src='https://stats.bahnzumberg.at/js/container_HjRnjBlM.js'; s.parentNode.insertBefore(g,s);
+    g.async=true; g.src='https://stats.bahnzumberg.at/js/container_ANAXmMKf.js'; s.parentNode.insertBefore(g,s);
   }, [])
+  
 
   return (
     <ThemeProvider theme={theme}>
