5bd37fdf13e3d013801431a512ab357254826bad
FALSAL <coconto.info@gmail.com>
2023-09-15 23:37:54 +0200

    test : correct the position of new condition

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.get([ '/*' ], function(req, res) {
 
 	console.log(" L34 Server.js : " + host);
 
-	if(host.indexOf('zuugle.si') >= 0) {
+	if(host.indexOf('zuugle.si') >= 0 || host === 'www2.zuugle.si') {
 		res.sendFile(path.join(__dirname, '../app/index-si.html'), (err) => {
 			if (err) {
 			  console.error('Error sending file:', err)
@@ -45,7 +45,7 @@ app.get([ '/*' ], function(req, res) {
 	);
 		
 	}
-	else if(host.indexOf('zuugle.it') >= 0 || host === 'www2.zuugle.si') {
+	else if(host.indexOf('zuugle.it') >= 0) {
 		res.sendFile(path.join(__dirname, '../app/index-it.html'));
 	}
 	else if(host.indexOf('zuugle.fr') >= 0) {
