e18a928e01564a3d825de2a1cc833f72243dd5f9
FALSAL <coconto.info@gmail.com>
2023-09-20 21:16:05 +0200

    delete legacy code

--- src/components/Search/AutosuggestSearch.js
+++ src/components/Search/AutosuggestSearch.js
@@ -1,48 +1,8 @@
-import React, { useState, useEffect } from "react";
-import Select, { components } from "react-select";
+import React, { useState } from "react";
 import { loadSuggestions } from "../../actions/crudActions";
-import { useTranslation } from "react-i18next";
-import { useTheme } from "@mui/material/styles";
-// import Async from "react-select/async";
-// import InputAdornment from "@mui/material/InputAdornment";
-// import SearchIcon from "../../icons/SearchIcon";
-// import ClearSearchIcon from "../../icons/ClearSearchIcon";
-import { styled } from "@mui/system";
 import CustomSelect from "./CustomSelect";
 import { isArray } from "lodash";
 
-const useStyles = styled("div")(({ theme }) => ({
-  "& label.Mui-focused": {
-    color: "#4992FF",
-  },
-  borderRadius: "20px",
-  border: "1.5px solid #DDD",
-  "& .MuiInput-underline:after": {
-    borderBottomColor: "#DDD",
-  },
-  "& .MuiOutlinedInput-root": {
-    "& fieldset": {
-      border: "0",
-    },
-    "&:hover fieldset": {
-      borderColor: "#DDD",
-    },
-    "&.Mui-focused fieldset": {
-      borderColor: "#DDD",
-      borderWidth: 0,
-    },
-    "& .MuiInputAdornment-root": {
-      position: "start",
-      "& svg": {
-        fill: theme.palette.text.primary,
-      },
-    },
-    "& .MuiInputAdornment-positionEnd": {
-      position: "end",
-      cursor: "pointer",
-    },
-  },
-}));
 
 const AutosuggestSearchTour = ({
   onSearchSuggestion,
@@ -52,14 +12,11 @@ const AutosuggestSearchTour = ({
   placeholder,
   goto,
 }) => {
-  // const [selectedOption, setSelectedOption] = useState(null);
-  const [input, setInput] = useState("");
-  // const { t } = useTranslation();
+
   const [options, setOptions] = useState([]);
   const urlSearchParams = new URLSearchParams(window.location.search);
   let searchParam = urlSearchParams.get("search");
   const [searchPhrase, setSearchPhrase] = useState(searchParam ?? "");
-  // const theme = useTheme();
 
   const handleSelect = (phrase) => {
     const value = phrase ? phrase : searchPhrase ? searchPhrase : "";
@@ -86,95 +43,7 @@ const AutosuggestSearchTour = ({
         });
     }
   };
-  const filterOptions = (inputValue) => {
-    return options.filter((i) =>
-      i.label.toLowerCase().includes(inputValue.toLowerCase())
-    );
-  };
-
-  const loadOptions = (inputValue, callback) => {
-    setTimeout(() => {
-      callback(filterOptions(inputValue));
-    }, 1000);
-  };
-
-  // const styles = {
-  //   control: (provided, state) => ({
-  //     ...provided,
-  //     background: "#fff",
-  //     borderColor: "#fff",
-  //     boxShadow: state.isFocused ? null : null,
-  //     textAlign: "left",
-  //     paddingLeft: "6px",
-  //     "&:hover": {
-  //       borderColor: "#fff",
-  //     },
-  //   }),
-  //   option: (provided) => ({
-  //     ...provided,
-  //     textAlign: "left",
-  //   }),
-  // };
-
-  const customStyles = {
-    // Style the container that holds the entire AsyncSelect component
-    container: (provided, state) => ({
-      ...provided,
-      width: "75%",
-      innerHeight: "80px",
-      borderRadius: "25px",
-    }),
-
-    // Style the control (input field and dropdown indicator)
-    control: (provided, state) => ({
-      ...provided,
-      borderColor: state.isFocused ? "#00A8E8" : "#CED4DA", // Example: change border color
-      boxShadow: state.isFocused ? "0 0 0 0.2rem rgba(0,168,232,.25)" : "none", // Example: add focus style
-    }),
-
-    // Style the option in the dropdown menu
-    option: (provided, state) => ({
-      ...provided,
-      backgroundColor: state.isSelected ? "#00A8E8" : "white", // Example: change selected option background color
-      color: state.isSelected ? "white" : "black", // Example: change selected option text color
-    }),
-
-    // Style the input field
-    input: (provided, state) => ({
-      ...provided,
-      color: "black", // Example: change text color
-    }),
-
-    // Style the loading indicator while fetching data asynchronously
-    loadingIndicator: (provided) => ({
-      ...provided,
-      color: "green", // Example: change loading indicator color
-    }),
-
-    // Style the clear indicator (X button)
-    clearIndicator: (provided) => ({
-      ...provided,
-      color: "red", // Example: change clear indicator color
-    }),
-  };
-
-  // useEffect(() => {
-  //   console.log("Input : ", input)
-  //   onSearchPhrase(input);
-  //   searchParam = urlSearchParams.get("search")
-  // }, [input]);
-
-
-
-  const NoOptionsMessage = (props) => {
-    return (
-      <components.NoOptionsMessage {...props}>
-        <span className="custom-css-class">No Suggestions</span>
-      </components.NoOptionsMessage>
-    );
-  };
-
-  // console.log(options);
+  
 
   return (
     <div>
