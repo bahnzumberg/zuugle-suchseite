95de3eb42d94b06252d55fb983c50d07f44c9fac
FALSAL <coconto.info@gmail.com>
2023-09-20 15:03:31 +0200

    same reason as previous commit

--- src/translations/indexHtmlTagTranslate.js
+++
@@ -1,7 +0,0 @@
-// Function to update the lang attribute of the html tag
-function updateHtmlLangAttribute(language) {
-  var htmlTag = document.getElementById("html-tag");
-  htmlTag.setAttribute("lang", language);
-  
-  // console.log("inside updateHtmlLangAttribute :", language);
-}
