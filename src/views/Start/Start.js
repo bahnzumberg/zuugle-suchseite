613728150b57f601743a6b47067f351636ff1b5d
AbdulGhaffar7 <2016n6497@gmail.com>
2023-10-12 13:28:35 +0500

    new changes

--- src/views/Start/Start.js
+++ src/views/Start/Start.js
@@ -286,13 +286,13 @@ function Start({
             </Box>
           </Box>
         )}
-        {!showMobileMenu && (
+        {/* {!showMobileMenu && (
           <FooterLinks links={listAllCityLinks(allCities, searchParams)} />
         )}
-        {/* {!showMobileMenu && (
+        {!showMobileMenu && (
           <FooterLinks links={listAllRangeLinks(allRanges, searchParams)} />
-        )}
-        {!showMobileMenu && <Footer />} */}
+        )} */}
+        {!showMobileMenu && <Footer />}
       </Box>
     );
   }
