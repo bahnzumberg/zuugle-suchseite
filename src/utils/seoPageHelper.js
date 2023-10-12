023c7bb5a21e632ef97c3d9ff03a0f0fb2fcc8ae
AbdulGhaffar7 <2016n6497@gmail.com>
2023-10-12 13:53:31 +0500

    new changes

--- src/utils/seoPageHelper.js
+++ src/utils/seoPageHelper.js
@@ -1,186 +1,208 @@
-import * as React from 'react';
+import * as React from "react";
 import Box from "@mui/material/Box";
-import {Grid, Typography} from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 // import {Helmet} from "react-helmet";
-import { Helmet, HelmetProvider } from 'react-helmet-async';
-import { useTranslation } from 'react-i18next';
-
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { useTranslation } from "react-i18next";
 
 //ToDo: Translate meta content in getPageHeader
 export const getPageHeader = (directLink) => {
-    const {t} = useTranslation();
+  const { t } = useTranslation();
 
-    //console.log("directLink :", directLink) // seems to be always on null value 
-    if(!!directLink && !!directLink.header){
-        return <HelmetProvider>
-            <Helmet>
-                <title>{directLink.header}</title>
-                <meta name="title" content={directLink.header} />
-                <meta name="description" content={directLink.description} />
-            </Helmet>
-        </HelmetProvider>
-    } else {
-        return <HelmetProvider>
-            <Helmet>
-                <title>{t("start.helmet_title")}</title>
-                <meta name="title" content="Zuugle - die Suchmaschine für Öffi-Bergtouren" />
-                <meta name="description" content="Zuugle zeigt dir geprüfte Verbindungen mit Bahn und Bus zu Bergtouren, Wanderungen, Skitouren, Schneeschuhwanderungen, etc. von deinem Wohnort aus an." />
-            </Helmet>
-        </HelmetProvider>
-    }
-}
+  //console.log("directLink :", directLink) // seems to be always on null value
+  if (!!directLink && !!directLink.header) {
+    return (
+      <HelmetProvider>
+        <Helmet>
+          <title>{directLink.header}</title>
+          <meta name="title" content={directLink.header} />
+          <meta name="description" content={directLink.description} />
+        </Helmet>
+      </HelmetProvider>
+    );
+  } else {
+    return (
+      <HelmetProvider>
+        <Helmet>
+          <title>{t("start.helmet_title")}</title>
+          <meta
+            name="title"
+            content="Zuugle - die Suchmaschine für Öffi-Bergtouren"
+          />
+          <meta
+            name="description"
+            content="Zuugle zeigt dir geprüfte Verbindungen mit Bahn und Bus zu Bergtouren, Wanderungen, Skitouren, Schneeschuhwanderungen, etc. von deinem Wohnort aus an."
+          />
+        </Helmet>
+      </HelmetProvider>
+    );
+  }
+};
 
 export const checkIfSeoPageCity = (location, cities) => {
-    //console.log("seoPageHelper: location : " + location) // /suche
-    //console.log("seoPageHelper: cities.length : " + cities.length) // 37 (exists)
-    if(!!location && !!location.pathname && location.pathname == "/suche"){
-        return null;
-    } else if(!!location && !!location.pathname && cities.length > 0){
-        //description:
-        // If location is null or pathname is null or undefined or the path is not a city path, the function returns null.
-        // If a city is found in the cities array that matches the value of the path name, the function returns an object representing the found city.
-        // If no city is found in the cities array that matches the value of the path name, the function returns undefined.
-        const found = cities.find(city => city.value == location.pathname.substring(1));
-        return found;
-    } else {
-        return null;
-    }
-}
+  //console.log("seoPageHelper: location : " + location) // /suche
+  //console.log("seoPageHelper: cities.length : " + cities.length) // 37 (exists)
+  if (!!location && !!location.pathname && location.pathname == "/suche") {
+    return null;
+  } else if (!!location && !!location.pathname && cities.length > 0) {
+    //description:
+    // If location is null or pathname is null or undefined or the path is not a city path, the function returns null.
+    // If a city is found in the cities array that matches the value of the path name, the function returns an object representing the found city.
+    // If no city is found in the cities array that matches the value of the path name, the function returns undefined.
+    const found = cities.find(
+      (city) => city.value == location.pathname.substring(1)
+    );
+    return found;
+  } else {
+    return null;
+  }
+};
 
 export const checkIfSeoPageRange = (location, ranges) => {
-    if(!!location && !!location.pathname && location.pathname == "/suche"){
-        return null;
-    } else if(!!location && !!location.pathname && ranges.length > 0){
-        const found = ranges.find(range => range.range_slug == parseRangeFromUrl(location.pathname.substring(1)));
-        return found;
-    } else {
-        return null;
-    }
-}
+  if (!!location && !!location.pathname && location.pathname == "/suche") {
+    return null;
+  } else if (!!location && !!location.pathname && ranges.length > 0) {
+    const found = ranges.find(
+      (range) =>
+        range.range_slug == parseRangeFromUrl(location.pathname.substring(1))
+    );
+    return found;
+  } else {
+    return null;
+  }
+};
 
 //description
 //This function, listAllCityLinks, takes in an array of cities and an optional searchParams object. It then maps over the array of cities and generates links for each city with appropriate URL parameters. Finally, it returns a JSX element containing a grid of city links wrapped in a Box with a Typography element for the title. If the cities argument is falsy, it returns an empty array.
 export const listAllCityLinks = (cities, searchParams = null) => {
-    const {t} = useTranslation();
+  const { t } = useTranslation();
 
-    const country = translatedCountry();
+  const country = translatedCountry();
 
-    if(!!cities){
-        const entries = cities.map((city,index) => {
-            let link = `${city.value}`;
-            if(!!searchParams && !!searchParams.get('p')){
-                link = `${link}?p=${searchParams.get('p')}`
-            }
-            return <Grid key={index} item xs={12} sm={6} md={4} ><a href={`/${link}`} className={"seo-city-link"}>{city.label}</a></Grid>
-        });
-        return <Box sx={{textAlign: "left"}}>
-                <Typography variant={"h4"} sx={{marginBottom: "20px"}}>
-                    <>                           
-                        {t('start.heimatbahnhoefe_in')} 
-                        {" "}
-                        {country}
-                    </> 
-                </Typography>
-            <Grid container>
-                {entries}
-            </Grid>
-        </Box>
-    }
-    return [];
-}
+  if (!!cities) {
+    const entries = cities.map((city, index) => {
+      let link = `${city.value}`;
+      if (!!searchParams && !!searchParams.get("p")) {
+        link = `${link}?p=${searchParams.get("p")}`;
+      }
+      return (
+        <Grid key={index} item xs={12} sm={6} md={4}>
+          <a href={`/${link}`} className={"seo-city-link"}>
+            {city.label}
+          </a>
+        </Grid>
+      );
+    });
+
+    return (
+      <Box sx={{ textAlign: "left" }}>
+        <Typography variant={"h4"} sx={{ marginBottom: "20px" }}>
+          <>
+            {t("start.heimatbahnhoefe_in")} {country}
+          </>
+        </Typography>
+        <Grid container>{entries}</Grid>
+      </Box>
+    );
+  }
+
+  return null; // Return null when there are no cities to render
+};
 
 export const listAllRangeLinks = (ranges, searchParams = null) => {
-    const {t} = useTranslation();
-    const country = translatedCountry();
+  const { t } = useTranslation();
+  const country = translatedCountry();
 
-    if(!!ranges){
-        const entries = ranges.map((range,index) => {
-            let city= '';
-            let link = `${range.range}`;
-            if (link == "null") {
-                return [];
-            }
-            link = parseRangeToUrl(link)
-            if(!!searchParams && !!searchParams.get('p')){
-                link = `${link}?p=${searchParams.get('p')}`
-            }
-            if(!!searchParams && !!searchParams.get('city')){
-                city = searchParams.get('city')
-            }
-            return <Grid key={index} item xs={12} sm={6} md={4}><a href={`/suche?range=${link}${!!city ? "&city=" + city : ""}`} className={"seo-city-link"}>{range.range}</a></Grid>
-        });
+  if (!!ranges) {
+    const entries = ranges.map((range, index) => {
+      let city = "";
+      let link = `${range.range}`;
+      if (link == "null") {
+        return [];
+      }
+      link = parseRangeToUrl(link);
+      if (!!searchParams && !!searchParams.get("p")) {
+        link = `${link}?p=${searchParams.get("p")}`;
+      }
+      if (!!searchParams && !!searchParams.get("city")) {
+        city = searchParams.get("city");
+      }
+      return (
+        <Grid key={index} item xs={12} sm={6} md={4}>
+          <a
+            href={`/suche?range=${link}${!!city ? "&city=" + city : ""}`}
+            className={"seo-city-link"}
+          >
+            {range.range}
+          </a>
+        </Grid>
+      );
+    });
 
-        return <Box sx={{textAlign: "left"}}>
-            <Typography variant={"h4"} sx={{marginBottom: "20px"}}>
-                <>                           
-                    {t('start.wanderdestinationen')}
-                    {" "} 
-                    {country}
-                </>  
-                </Typography>
-            <Grid container>
-                {entries}
-            </Grid>
-        </Box>
-    }
-    return [];
-}
+    return (
+      <Box sx={{ textAlign: "left" }}>
+        <Typography variant={"h4"} sx={{ marginBottom: "20px" }}>
+          <>
+            {t("start.wanderdestinationen")} {country}
+          </>
+        </Typography>
+        <Grid container>{entries}</Grid>
+      </Box>
+    );
+  }
+  return [];
+};
 
+const translatedCountry = () => {
+  const { t } = useTranslation();
+  const country = getCountryName();
+  const countryKey = getCountryKey(country);
 
-const translatedCountry =()=>{
-
-    const {t} = useTranslation();
-    const country =  getCountryName(); 
-    const countryKey = getCountryKey(country);
-    
-    return t(`start.${countryKey}`);
-}
-
-
+  return t(`start.${countryKey}`);
+};
 
 const getCountryKey = (name) => {
-
-    switch (name) {
-        case "Schweiz":
-            return "schweiz";
-        case "Österreich":
-            return "oesterreich";
-        case "Deutschland":
-            return "deutschland";
-        case "Frankreich":
-            return "frankreich";
-        case "Slowenien":
-            return "slowenien";
-        case "Italien":
-            return "italien";
-        default:
-            return "oesterreich";
-    }
-}
+  switch (name) {
+    case "Schweiz":
+      return "schweiz";
+    case "Österreich":
+      return "oesterreich";
+    case "Deutschland":
+      return "deutschland";
+    case "Frankreich":
+      return "frankreich";
+    case "Slowenien":
+      return "slowenien";
+    case "Italien":
+      return "italien";
+    default:
+      return "oesterreich";
+  }
+};
 
 const getCountryName = () => {
-    let host = window.location.host;
-    // let host = "www2.zuugle.fr";
-    
-    if(host.indexOf('zuugle.ch') >= 0){
-        return "Schweiz"
-    } else if(host.indexOf('zuugle.de') >= 0){
-        return "Deutschland"
-    } else if(host.indexOf('zuugle.it') >= 0){
-        return "Italien"
-    } else if(host.indexOf('zuugle.fr') >= 0){
-        return "Frankreich"
-    } else if(host.indexOf('zuugle.si') >= 0){
-        return "Slowenien"
-    } else {
-        return "Österreich";
-    }
-}
+  let host = window.location.host;
+  // let host = "www2.zuugle.fr";
+
+  if (host.indexOf("zuugle.ch") >= 0) {
+    return "Schweiz";
+  } else if (host.indexOf("zuugle.de") >= 0) {
+    return "Deutschland";
+  } else if (host.indexOf("zuugle.it") >= 0) {
+    return "Italien";
+  } else if (host.indexOf("zuugle.fr") >= 0) {
+    return "Frankreich";
+  } else if (host.indexOf("zuugle.si") >= 0) {
+    return "Slowenien";
+  } else {
+    return "Österreich";
+  }
+};
 
 const parseRangeFromUrl = (text) => {
-    return decodeURI(text);
-}
+  return decodeURI(text);
+};
 
 const parseRangeToUrl = (text) => {
-    return encodeURI(text);
-}
+  return encodeURI(text);
+};
