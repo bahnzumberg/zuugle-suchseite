3ccd0b469b2ab44699e00ee47625ba44072fbece
FALSAL <coconto.info@gmail.com>
2023-11-13 14:13:18 +0100

    creating pdf causes authentication issue, check reverting to no authentication

--- apps/backend/src/middlewares/authenticate.js
+++ apps/backend/src/middlewares/authenticate.js
@@ -1,11 +1,13 @@
 
- export default (req, res, next) => {
-     const authorizationHeader = req.headers['authorization'];
-     if ((authorizationHeader && authorizationHeader === "FV69pR5PQQLcQ4wuMtTSqKqyYqf5XEK4")) {
-         next();
-     } else {
-         res.status(403).json({
-             error: "not authenticated"
-         });
-     }
- }
+//  export default (req, res, next) => {
+//      const authorizationHeader = req.headers['authorization'];
+//      if ((authorizationHeader && authorizationHeader === "FV69pR5PQQLcQ4wuMtTSqKqyYqf5XEK4")) {
+//          next();
+//      } else {
+//          res.status(403).json({
+//              error: "not authenticated"
+//          });
+//      }
+//  }
+
+ export default (req, res, next) => { next(); }
