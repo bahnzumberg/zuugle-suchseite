3676f701e02504f37c45e867f0d2282e6a6cd9e0
Martin Heppner <105971399+martinheppner@users.noreply.github.com>
2023-12-30 21:46:47 +0700

    No cache buster output line

--- src/App.js
+++ src/App.js
@@ -74,7 +74,6 @@ if(!localStorage.getItem('visited')) {
       currentVersion={version}
       isEnabled={isProduction} //If false, the library is disabled.
       isVerboseMode={false} //If true, the library writes verbose logs to console.
-      loadingComponent={<LoadingVersionCheck />} //If not pass, nothing appears at the time of new version check.
       metaFileDirectory={'.'} //If public assets are hosted somewhere other than root on your server.
     >
 
