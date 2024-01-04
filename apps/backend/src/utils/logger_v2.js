5ae4b7c6d34bff7d6adcadc6a8c752c395b8b7d9
FALSAL <coconto.info@gmail.com>
2024-01-04 11:48:59 +0100

    add a reset to log file

--- apps/backend/src/utils/logger_v2.js
+++ apps/backend/src/utils/logger_v2.js
@@ -1,6 +1,9 @@
 import * as fs from 'fs';
 import path from 'path';
 
+let lineCount = 0;
+const maxAllowed = 100;
+
 export default function (text) {
     const proddevPath = process.env.NODE_ENV !== 'production' ? '../../' : '../';
     const filePath = path.join(__dirname, proddevPath, 'logs/api.log');
@@ -10,7 +13,15 @@ export default function (text) {
         fs.mkdirSync(path.dirname(filePath), { recursive: true });
     }
 
+if (lineCount < maxAllowed) {
+    // add log entry
     fs.appendFileSync(filePath, text + '\n');
+    lineCount++;
+} else {
+    // this will erase the current file and add a new "text" line
+    fs.writeFileSync(filePath, text + '\n');
+    lineCount = 1;
+}
 }
 
 // call this function whenever the server starts// for now test it on tours.js
