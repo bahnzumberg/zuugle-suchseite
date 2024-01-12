a936f61ce7fe7898a9115bcb3cbfb171918800d5
FALSAL <coconto.info@gmail.com>
2024-01-12 21:05:20 +0100

    fix logger functionality inside tourPdfWrapper

--- apps/backend/src/routes/tours.js
+++ apps/backend/src/routes/tours.js
@@ -1321,12 +1321,14 @@ const tourPdfWrapper = async (req, res) => {
         res.status(404).json({success: false});
         return;
     }else{
-        logger("-----------------------------------------------")
-        logger(`L1324 : tour with id ${id} found`)
-        logger("-----------------------------------------------")
-        console.log("-----------------------------------------------")
-        console.log(`L1324 : tour with id ${id} found`)
-        console.log("-----------------------------------------------")
+        if(process.env.NODE_ENV != "production"){
+            logger("-----------------------------------------------")
+            logger(`L1324 : tour with id ${id} found`)
+            logger("-----------------------------------------------")
+            console.log("-----------------------------------------------")
+            console.log(`L1324 : tour with id ${id} found`)
+            console.log("-----------------------------------------------")
+        }
     }
     if(!!connectionId){
         connection = await knex('fahrplan').select().where({id: connectionId}).first();
