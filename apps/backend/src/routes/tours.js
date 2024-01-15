cd378ce72114d1f1519553bec8aa426978f62b82
FALSAL <coconto.info@gmail.com>
2024-01-15 20:47:13 +0100

    again with a new logger

--- apps/backend/src/routes/tours.js
+++ apps/backend/src/routes/tours.js
@@ -1325,14 +1325,14 @@ const tourPdfWrapper = async (req, res) => {
         res.status(404).json({success: false});
         return;
     }else{
-        if(process.env.NODE_ENV != "production"){
-            logger("-----------------------------------------------")
-            logger(`L1324 : tour with id ${id} found`)
-            logger("-----------------------------------------------")
-            console.log("-----------------------------------------------")
-            console.log(`L1324 : tour with id ${id} found`)
-            console.log("-----------------------------------------------")
-        }
+        logger(`L1324 : tour with id ${id} found`)
+        // if(process.env.NODE_ENV != "production"){
+        //     logger("-----------------------------------------------")
+        //     logger("-----------------------------------------------")
+        //     console.log("-----------------------------------------------")
+        //     console.log(`L1324 : tour with id ${id} found`)
+        //     console.log("-----------------------------------------------")
+        // }
     }
     if(!!connectionId){
         connection = await knex('fahrplan').select().where({id: connectionId}).first();
