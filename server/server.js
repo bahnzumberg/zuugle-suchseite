da37b2d64223acae3d9a5df5ab2a166e0a93e5a9
FALSAL <coconto.info@gmail.com>
2024-01-16 14:58:10 +0100

    adjust config figures for rate-limiter

--- server/server.js
+++ server/server.js
@@ -11,8 +11,8 @@ var port = 4000;
 const app = express();
 
 const limiter = RateLimit({
-	windowMs: 1 * 60 * 1000, // 1 minute
-	limit: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	windowMs: 15 * 60 * 1000, // 15 minute
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 	// store: ... , // Use an external store for consistency across multiple server instances.
