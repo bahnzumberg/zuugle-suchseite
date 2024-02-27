2cc12fcc1f7d550fe39ffa64dc782dc69efc29b9
Martin Heppner <105971399+martinheppner@users.noreply.github.com>
2024-02-27 06:05:13 +0100

    deactivate logger on prod

--- apps/backend/src/utils/logger.js
+++ apps/backend/src/utils/logger.js
@@ -3,9 +3,11 @@ import path from 'path';
 
 // TESTING DETACHED HEAD STATE 
 export default function (text) {
-    const onoffswitch = 'on';
+    const onoffswitch = 'off';  // values: on / off
 
-    if(onoffswitch == 'on') {
+    if(onoffswitch == 'on' || process.env.NODE_ENV !== 'production') {
+        // Either onoffswitch is set to 'on' or we are not on prod or uat
+
         const proddevPath = process.env.NODE_ENV !== 'production' ? '../../' : '../../';
         const filePath = path.join(__dirname, proddevPath, 'logs/api.log');
 
@@ -14,34 +16,21 @@ export default function (text) {
             fs.mkdirSync(path.dirname(filePath), { recursive: true });
         }
 
+        // Create file when not existent yet
+        if (!fs.existsSync(filePath)) {
+            fs.writeFileSync(filePath, '');
+        }
+
         let date_ob = new Date();
-        let date = ("0" + date_ob.getDate()).slice(-2);
+        let day = ("0" + date_ob.getDate()).slice(-2);
         let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
         let year = date_ob.getFullYear();
         let hours = date_ob.getHours();
         let minutes = date_ob.getMinutes();
         let seconds = date_ob.getSeconds();
-        let log_date_time = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds + " ";
+        let log_date_time = year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds + " ";
 
-        console.log(log_date_time)
         // add log entry
         fs.appendFileSync(filePath, log_date_time + text + '\n'); 
-    }
-}
-
-// call this function whenever the server starts// for now test it on tours.js
-export function create_api_log() {
-    // const proddevPath = process.env.NODE_ENV !== 'production' ? '../../' : '../';
-    const proddevPath = process.env.NODE_ENV !== 'production' ? '../../' : '../../';
-    const filePath = path.join(__dirname, proddevPath, 'logs/api.log');
-
-    // Make sure directory exists
-    if (!fs.existsSync(path.dirname(filePath))) {
-        fs.mkdirSync(path.dirname(filePath), { recursive: true });
-    }
-
-    // Create file when not existent yet
-    if (!fs.existsSync(filePath)) {
-        fs.writeFileSync(filePath, '');
     }
 }
