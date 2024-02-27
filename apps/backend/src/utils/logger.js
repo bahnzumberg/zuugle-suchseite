03f8ea2e32e88e3f6d7d311289e839c1a304b48b
Martin Heppner <105971399+martinheppner@users.noreply.github.com>
2024-02-27 05:02:39 +0100

    Added switch on/off and datetime to logger function

--- apps/backend/src/utils/logger.js
+++ apps/backend/src/utils/logger.js
@@ -1,30 +1,31 @@
 import * as fs from 'fs';
 import path from 'path';
 
-let lineCount = 0;
-const maxAllowed = 100;
 // TESTING DETACHED HEAD STATE 
 export default function (text) {
+    $onoffswitch = 'on';
 
-    // const proddevPath = process.env.NODE_ENV !== 'production' ? '../../' : '../';
-    if(process.env.NODE_ENV !== 'production')
-    {const proddevPath = process.env.NODE_ENV !== 'production' ? '../../' : '../../';
-    const filePath = path.join(__dirname, proddevPath, 'logs/api.log');
+    if($onoffswitch == 'on') {
+        const proddevPath = process.env.NODE_ENV !== 'production' ? '../../' : '../../';
+        const filePath = path.join(__dirname, proddevPath, 'logs/api.log');
 
-    // Ensure the directory exists
-    if (!fs.existsSync(path.dirname(filePath))) {
-        fs.mkdirSync(path.dirname(filePath), { recursive: true });
+        // Ensure the directory exists
+        if (!fs.existsSync(path.dirname(filePath))) {
+            fs.mkdirSync(path.dirname(filePath), { recursive: true });
+        }
+
+        let date_ob = new Date();
+        let date = ("0" + date_ob.getDate()).slice(-2);
+        let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+        let year = date_ob.getFullYear();
+        let hours = date_ob.getHours();
+        let minutes = date_ob.getMinutes();
+        let seconds = date_ob.getSeconds();
+        let log_date_time = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds + " ";
+
+        // add log entry
+        fs.appendFileSync(filePath, log_date_time + text + '\n'); 
     }
-
-if (lineCount < maxAllowed) {
-    // add log entry
-    fs.appendFileSync(filePath, text + '\n');
-    lineCount++;
-} else {
-    // this will erase the current file and add a new "text" line
-    fs.writeFileSync(filePath, text + '\n');
-    lineCount = 1;
-}}
 }
 
 // call this function whenever the server starts// for now test it on tours.js
