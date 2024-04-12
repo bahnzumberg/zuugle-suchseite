940c9a3a7ab8d0f113453061a53763f469756f3f
FALSAL <coconto.info@gmail.com>
2024-04-12 10:59:12 +0200

    complete button style

--- src/components/Search/MapBtn.js
+++ src/components/Search/MapBtn.js
@@ -1,27 +1,35 @@
 import * as React from "react";
 import Button from '@mui/material/Button';
-
+import MapIcon from "../../icons/Map";
 
 const MapBtn = ({ children, onClick }) => {
-    return (
-      <Button sx={{
-        position: "fixed", 
-        bottom: "20px", // Adjust bottom position for desktop
-        // left: "50%", // Center horizontally
-        transform: "translateX(-50%)", // Center horizontally
-        width: "170px", 
-        height: "40px", 
-        borderRadius: "50px 50px ",
-        opacity: 1, 
+  return (
+    <Button
+      sx={{
+        position: "fixed",
+        bottom: "20px",
+        transform: "translateX(-50%)",
+        width: "200px",
+        height: "40px",
+        borderRadius: "50px 50px",
+        opacity: 1,
         backgroundColor: "#101010",
-        color: "#f1f1f1", // text color
+        color: "#f1f1f1",
         cursor: "pointer",
+        alignItems: "center", // Align vertically
+        justifyContent: "space-between", // Space content evenly
+        flexDirection: "row", // Text and icon in a row
+        margin: "2 auto",
       }}
       onClick={onClick}
-      >
+      aria-label="contained"
+      variant="contained"
+      endIcon={<MapIcon />}
+    >
+      <span style={{ paddingLeft: "10px" }}>
         {children}
-      </Button>
-    );
-  };
-  
+      </span> 
+    </Button>
+  );
+};
   export default MapBtn;
