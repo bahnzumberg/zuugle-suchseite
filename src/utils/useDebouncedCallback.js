6b6edf0193ea247e5035c1078aa87b35b14cfb8f
FALSAL <coconto.info@gmail.com>
2024-04-18 15:47:58 +0200

    change MyComponent to fit the new useCallback in custom hook

--- src/utils/useDebouncedCallback.js
+++ src/utils/useDebouncedCallback.js
@@ -1,22 +1,26 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState, useCallback } from 'react';
 
 export default function useDebouncedCallback(callback, delay) {
-  const [debouncedValue, setDebouncedValue] = useState(null);
+  const [debouncedValue, setDebouncedValue] = useState();
 
   const timer = useRef(null);
 
-  useEffect(() => {
-    if (timer.current) {
-      clearTimeout(timer.current);
-    }
+  const debouncedFunction = useCallback(
+    (...args) => {
+      if (timer.current) {
+        clearTimeout(timer.current);
+      }
+      
+      timer.current = setTimeout(() => {
+        setDebouncedValue(callback(...args));
+      }, delay);
 
-    timer.current = setTimeout(() => {
-      setDebouncedValue(callback);
-    }, delay);
-
-    // Cleanup function to clear timeout on unmount
-    return () => clearTimeout(timer.current);
-  }, [callback, delay]);
-
-  return debouncedValue;
+      console.log("L18 inside debounceFunction", debouncedValue)
+      // Cleanup function to clear timeout on unmount
+      return () => clearTimeout(timer.current);
+    },
+    [callback, delay]
+  );
+  console.log("L22 before return debouncedFunction", debouncedFunction)
+  return {debouncedFunction, debouncedValue};
 }
