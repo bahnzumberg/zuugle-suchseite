9a8804d7781c03b706bda9179c8d279aefb72e8b
FALSAL <coconto.info@gmail.com>
2024-05-05 23:54:59 +0200

    fix syntax err

--- apps/backend/src/jobs/sync.js
+++ apps/backend/src/jobs/sync.js
@@ -249,7 +249,7 @@ async function _syncConnectionGPX(key, fileName, title, mod=null){
                     console.log("track_point_lon" , row.track_point_lon)
                     console.log("track_point_lat" , row.track_point_lat)
                     console.log("track_point_elevation" , row.track_point_elevation)
-                }
+                })
                 // trackPoints.forEach((row) => {
                 //     if(row.track_point_sequence == 1){
                 //         knex.raw(`INSERT INTO tracks (track_key,track_point_sequence,track_point_lon,track_point_lat,track_point_elevation) VALUES 
