eb856c3b9a1c00bc7f4c946a2cd4b2e09276f3e5
FALSAL <coconto.info@gmail.com>
2024-05-13 07:46:52 +0200

    use start in language files instead of Start, replace name_space for map btn translation with new one

--- src/components/Search/MapBtn.js
+++ src/components/Search/MapBtn.js
@@ -21,7 +21,7 @@ const MapBtn = ({ children, onClick, showMap}) => {
 
   useEffect(() => {
     // Update button text when language changes
-    setMapBtnText(showMap ? t("main_only.kartenansicht_entfernen") : t("start_main_pages.zur_kartenansicht"));
+    setMapBtnText(showMap ? t("Main.kartenansicht_entfernen") : t("Main.zur_kartenansicht"));
   }, [showMap, t]);
   
 
