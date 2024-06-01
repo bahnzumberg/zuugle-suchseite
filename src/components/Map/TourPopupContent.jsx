b2520b140ae2d8655fe094f7581dcbe26228ed06
FALSAL <coconto.info@gmail.com>
2024-06-01 14:17:04 +0200

    receive the props handler and Marker id, change the path to tested image

--- src/components/Map/TourPopupContent.jsx
+++ src/components/Map/TourPopupContent.jsx
@@ -3,14 +3,17 @@ import {Popup} from 'react-leaflet';
 import { popupContent, popupHead, popupImage } from "./popupStyles.js";
 import MountinImage from './MountinImage.jsx';
 
-const TourPopupContent = () => (
-  <Popup className="request-popup">
+const TourPopupContent = (handlePopupClick, tourId) => (
+  <Popup 
+    className="request-popup"
+    // key={} //Check if poupid is same as markerid ?
+  >
     <div
       className={popupContent}
-      // onClick={e => handlePopupClick(e,mark.id)}
+      // onClick={e => handlePopupClick(e,tourId)}
     >
       <MountinImage
-        imageUrl="public/img/mountain-landscape-2031539_640.jpg"
+        imageUrl="url(/app_static/img/mountain-landscape-2031539_640.jpg)"
         // style={{
         //   width: "200px",
         //   height: "100px",
