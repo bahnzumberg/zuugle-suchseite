ae83a713eee97686cc510fed44aa81d60d23185b
FALSAL <coconto.info@gmail.com>
2024-06-02 17:03:11 +0200

    remove unnecessary code

--- src/components/Map/TourPopupContent.jsx
+++ src/components/Map/TourPopupContent.jsx
@@ -6,19 +6,13 @@ import MountinImage from './MountinImage.jsx';
 const TourPopupContent = (handlePopupClick, tourId) => (
   <Popup 
     className="request-popup"
-    // key={} //Check if poupid is same as markerid ?
   >
     <div
       className={popupContent}
-      // onClick={e => handlePopupClick(e,tourId)}
+      // onClick={()=>handlePopupClick()}
     >
       <MountinImage
         imageUrl="app_static/img/mountain-landscape-2031539_640.jpg"
-        // style={{
-        //   width: "200px",
-        //   height: "100px",
-        //   marginBottom: "10px", // Add some margin for spacing
-        // }}
         className={popupImage}
       />
       <div className={`m-2 ${popupHead}`}>
