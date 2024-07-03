767bb5cc9cadb7dc4041e5248a1591b56d8951ed
FALSAL <falsalih@aol.de>
2024-07-03 12:47:24 +0200

    Fix buggy code in TourCardContainer.js

--- src/components/TourCardContainer.js
+++ src/components/TourCardContainer.js
@@ -56,6 +56,10 @@ export default function TourCardContainer({
     }
   }, [hasMore, filterValues, searchParams]);
 
+  useEffect(() => {
+      // console.log("L40 markersChanged :", markersChanged);
+      let bounds = mapBounds ? JSON.stringify(mapBounds) : '';
+      if (mapBounds && markersChanged) {
 
       // eslint-disable-next-line react-hooks/exhaustive-deps
       loadTours({
