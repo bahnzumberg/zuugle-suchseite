2c50a30c2bc144bc54bd97e8d0062cd84a28a09b
Martin Heppner <105971399+martinheppner@users.noreply.github.com>
2024-07-04 17:25:39 +0200

    getDurationValue(values.min_best_connection_duration);

--- apps/backend/src/jobs/sync.js
+++ apps/backend/src/jobs/sync.js
@@ -809,7 +809,7 @@ export async function mergeToursWithFahrplan(){
                         .first();
                     
                     if (!!values)  {
-                        fp.best_connection_duration = getDurationValue(values.min_best_connection_duration, "HH:mm:ss");
+                        fp.best_connection_duration = getDurationValue(values.min_best_connection_duration);
                         fp.connection_no_of_transfers = values.min_connection_no_of_transfers;
                     }
                     else {
