1d768ce2fed9e1785c150ac0627ebf012aa14bec
Martin Heppner <105971399+martinheppner@users.noreply.github.com>
2024-07-04 08:21:23 +0200

    Optional Params

--- src/App.js
+++ src/App.js
@@ -74,9 +74,7 @@ function App() {
               <Route path="/total" element={<Start />} />
               <Route path="/suche" element={<Main />} />
               <Route path="/about" element={<About />} />
-              <Route path="/tour" element={<DetailReworked />} >
-                <Route path=":id/:city" element={<DetailReworked />} />
-              </Route>
+              <Route path="/tour/:id?/:city?" element={<DetailReworked />} />
               <Route path="/provider/:provider" element={<DetailReworked />} />
               <Route path="/imprint" element={<Impressum />} />
               <Route path="/privacy" element={<Privacy />} />
