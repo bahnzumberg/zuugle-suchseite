040ab826edfbd4450673019858ae7b7af5af50d4
Martin Heppner <105971399+martinheppner@users.noreply.github.com>
2024-07-22 00:16:09 +0200

    console.logs entfernt

--- src/components/TourCardContainer.js
+++ src/components/TourCardContainer.js
@@ -42,9 +42,6 @@ export default function TourCardContainer({
   let sort = searchParams.get("sort");
   let map = searchParams.get("map");
   let provider = searchParams.get("p");
-
-  console.log(`TourCardContainer searchParams.get("p"): `, searchParams.get("p"))
-  console.log(`TourCardContainer provider: `, provider)
   
   useEffect(() => {
     if (!!hasMore && !!filterValues) {
