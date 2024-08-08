d4993bfb798e596314f8b5d0863c03005d5078ac
FALSAL <coconto.info@gmail.com>
2024-08-08 18:54:11 +0200

    main page: center the card container

--- src/components/TourCardContainer.js
+++ src/components/TourCardContainer.js
@@ -18,6 +18,7 @@ export default function TourCardContainer({
   filterValues,
   markersChanged,
   mapBounds,
+  isMobile
 }) {
   const [searchParams, setSearchParams] = useSearchParams();
   const [hasMore, setHasMore] = useState(true);
@@ -107,11 +108,10 @@ export default function TourCardContainer({
         dataLength={tours.length}
         next={_loadTours}
         hasMore={true}
-        // hasMore={hasMore}
         loader={!!loading && <CircularProgress />}
         endMessage={<EndOfList />}
       >
-        <Grid container spacing={2}>
+        <Grid container spacing={2} style={{  marginLeft: `${!isMobile ? '35px' : null}` }}>
           {tours.map((tour, index) => (
             <Grid
               key={index}
