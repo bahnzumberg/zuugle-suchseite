d5df77810d4f906f49077cc17a4af13e992e7464
shiv <shivfreelance1994@gmail.com>
2024-09-05 20:06:54 +0530

    loading issue fix

--- src/components/Search/AutosuggestSearch.js
+++ src/components/Search/AutosuggestSearch.js
@@ -1,53 +1,48 @@
 import React, { useState } from "react";
 import { loadSuggestions } from "../../actions/crudActions";
 import CustomSelect from "./CustomSelect";
-import { isArray } from "lodash";
 
+const AutosuggestSearchTour = ({ onSearchSuggestion, city, language }) => {
+	const [options, setOptions] = useState([]);
+	const urlSearchParams = new URLSearchParams(window.location.search);
+	let searchParam = urlSearchParams.get("search");
+	const [searchPhrase, setSearchPhrase] = useState(searchParam ?? "");
 
-const AutosuggestSearchTour = ({
-  onSearchSuggestion,
-  city,
-  language,
-}) => {
+	const handleSelect = (phrase) => {
+		const value = phrase ? phrase : searchPhrase ? searchPhrase : "";
+		onSearchSuggestion(value);
+	};
 
-  const [options, setOptions] = useState([]);
-  const urlSearchParams = new URLSearchParams(window.location.search);
-  let searchParam = urlSearchParams.get("search");
-  const [searchPhrase, setSearchPhrase] = useState(searchParam ?? "");
+	//What the component should do while I type in values
+	const handleInputChange = (inputValue) => {
+		if (city !== null) {
+			setSearchPhrase(inputValue);
+			loadSuggestions(inputValue, city.value, language) //Call the backend
+				.then((suggestions) => {
+					const newOptions = suggestions?.map((suggestion) => ({
+						//Get the New suggestions and format them the correct way
+						label: suggestion.suggestion,
+						value: suggestion.suggestion,
+					}));
+					newOptions &&
+						Array.isArray(newOptions) &&
+						setOptions([...newOptions]);
+				})
+				.catch((err) => {
+					console.error(err);
+				});
+		}
+	};
 
-  const handleSelect = (phrase) => {
-    const value = phrase ? phrase : searchPhrase ? searchPhrase : "";
-    onSearchSuggestion(value);
-  };
-
-  //What the component should do while I type in values
-  const handleInputChange = (inputValue) => {
-    if (city !== null) {
-      setSearchPhrase(inputValue);
-      loadSuggestions(inputValue, city.value, language) //Call the backend
-        .then((suggestions) => {
-          const newOptions = suggestions?.map((suggestion) => ({
-            //Get the New suggestions and format them the correct way
-            label: suggestion.suggestion,
-            value: suggestion.suggestion,
-          }));
-          newOptions && isArray(newOptions) && setOptions([...newOptions]);
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    }
-  };
-  
-  return (
-    <div>
-      <CustomSelect
-        options={options}
-        handleSelect={handleSelect}
-        handleInputChange={handleInputChange}
-        searchPhrase={searchPhrase}
-      />
-    </div>
-  );
+	return (
+		<div>
+			<CustomSelect
+				options={options}
+				handleSelect={handleSelect}
+				handleInputChange={handleInputChange}
+				searchPhrase={searchPhrase}
+			/>
+		</div>
+	);
 };
 export default AutosuggestSearchTour;
