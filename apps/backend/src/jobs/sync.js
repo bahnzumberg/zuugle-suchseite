467c24c81fecabc340998c6797d6234be7524a08
Martin Heppner <105971399+martinheppner@users.noreply.github.com>
2025-09-05 15:16:25 +0200

    pLimit

--- apps/backend/src/jobs/sync.js
+++ apps/backend/src/jobs/sync.js
@@ -579,15 +579,16 @@ export async function syncGPX(){
 
     let allTours = null;
     let promises = null;
+    const pLimit = require('p-limit');
+    const limit  = pLimit(10); // Limit to 10 concurrent connections
+
     for (let i=0; i<10; i++) {
         console.log(moment().format('HH:mm:ss'), ' Creating gpx files - step '+i);
         allTours = await knex('tour').select(["title", "id", "hashed_url"]).whereRaw("MOD(id, 10)="+i)
               
         if(!!allTours && allTours.length > 0){
             try {
-                promises = allTours.map(entry => {
-                    return _syncGPX(entry.id, entry.hashed_url, entry.title);
-                });
+                const promises = allTours.map(entry => limit(() => _syncGPX(entry.id, entry.hashed_url, entry.title)));
                 await Promise.all(promises);
             }
             catch(e) {
