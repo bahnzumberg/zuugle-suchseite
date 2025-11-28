72109785f404f2a5fe0bba38e4925cdd918013bd
Christina Kirk <christina.kirk@proton.me>
2025-11-28 21:30:11 +0100

    Use eslint-config-prettier as recommended by prettier
    https://prettier.io/docs/integrating-with-linters

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import tseslint from "typescript-eslint";
-import prettier from "eslint-plugin-prettier/recommended";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
@@ -47,10 +47,5 @@ export default defineConfig([
       },
     },
   },
-  prettier,
-  {
-    rules: {
-      "prettier/prettier": "warn",
-    },
-  },
+  eslintConfigPrettier,
 ]);
