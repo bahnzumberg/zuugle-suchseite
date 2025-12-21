f3fb1a11336c51dd22c7544197910af671a607d3
Martin Heppner <105971399+martinheppner@users.noreply.github.com>
2025-12-21 14:19:48 +0100

    Revert "Performance improvements for homepage LCP and Fonts"

--- src/views/Start/BackgroundImageLoader.tsx
+++ src/views/Start/BackgroundImageLoader.tsx
@@ -1,4 +1,8 @@
 import { Box } from "@mui/material";
+import { isMobileDevice } from "../../utils/globals";
+
+const LINEAR_GRADIENT =
+  "linear-gradient(rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0.45)), ";
 
 export interface BackgroundImageLoaderProps {
   tld: string;
@@ -11,56 +15,21 @@ const BackgroundImageLoader = ({
   sx,
   children,
 }: BackgroundImageLoaderProps) => {
+  const backgroundImage = isMobileDevice()
+    ? `${LINEAR_GRADIENT} url(https://cdn.zuugle.at/img/background_start_mobil_${tld}.webp)`
+    : `${LINEAR_GRADIENT} url(https://cdn.zuugle.at/img/background_start_small_${tld}.webp)`;
+
   return (
     <Box
       className={"header-container"}
       sx={{
-        position: "relative",
-        overflow: "hidden",
+        backgroundImage: backgroundImage,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
         ...sx,
       }}
     >
-      <Box
-        sx={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          zIndex: 0,
-        }}
-      >
-        <picture>
-          <source
-            media="(max-width: 900px)"
-            srcSet={`https://cdn.zuugle.at/img/background_start_mobil_${tld}.webp`}
-          />
-          <img
-            src={`https://cdn.zuugle.at/img/background_start_small_${tld}.webp`}
-            alt="Background"
-            style={{
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-            }}
-            fetchPriority="high"
-          />
-        </picture>
-        <div
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            background:
-              "linear-gradient(rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0.45))",
-          }}
-        />
-      </Box>
-      <Box sx={{ position: "relative", zIndex: 1, height: "100%" }}>
-        {children}
-      </Box>
+      {children}
     </Box>
   );
 };
