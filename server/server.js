1528aa044ae19c6d3254f6ebcbb0c2aedaeca939
Christina Kirk <christina.kirk@proton.me>
2025-12-21 10:36:59 +0100

    Update server.js with security headers and ESM
    - Modernize server/server.js to ES Modules and add helmet for security headers.
    - Update http-proxy-middleware usage in server.js.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import RateLimit from "express-rate-limit";
+import express from "express";
+import path from "path";
+import compression from "compression";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import helmet from "helmet";
+import { fileURLToPath } from "url";
 
-var express = require("express"),
-  path = require("path"),
-  compression = require("compression"),
-  proxy = require("http-proxy-middleware");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 var port = 4000;
 const app = express();
@@ -13,8 +17,10 @@ const limiter = RateLimit({
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
   standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-  // store: ... , // Use an external store for consistency across multiple server instances.
 });
+
+// Use Helmet for security headers
+app.use(helmet());
 
 // Apply the rate limiting middleware to all requests.
 app.use(limiter);
@@ -32,16 +38,16 @@ app.use(
   "/app_static",
   express.static(path.join(__dirname, "../app/app_static")),
 );
-app.use("/api", proxy({ target: "http://localhost:6060", secure: false }));
+app.use(
+  "/api",
+  createProxyMiddleware({ target: "http://localhost:6060", secure: false }),
+);
 app.use(
   "/public",
   express.static(path.join(__dirname, "../api/public"), { fallthrough: false }),
 );
 
 app.use((req, res, next) => {
-  //res.append('Access-Control-Allow-Origin', ['*']);
-  //res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  //res.append('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
