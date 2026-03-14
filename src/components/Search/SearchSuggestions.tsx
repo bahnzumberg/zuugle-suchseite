0ee8dcb0e11e4897d14bca4d6557457279300904
Ranjeet <68504990+ranjiitk121@users.noreply.github.com>
2026-03-14 14:14:02 +0100

    New autocomplete search with debounce (#786)
    See #760
    
    * added new autocomplete with debounce
    
    * renamed types, moved suggestion to new component and new icons
    
    * added effect to cancel debounce, reset suggestion on change and type changes in suggestions component
    
    * return cleanup fn in use effect

---
+++ src/components/Search/SearchSuggestions.tsx
@@ -0,0 +1,69 @@
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Skeleton from "@mui/material/Skeleton";
+import CabinOutlined from "@mui/icons-material/CabinOutlined";
+import PeakIcon from "./icons/PeakIcon";
+import LandscapeOutlined from "@mui/icons-material/LandscapeOutlined";
+import SearchOutlined from "@mui/icons-material/SearchOutlined";
+import {
+  AutocompleteSearchSuggestion,
+  AutocompleteSuggestionType,
+} from "../../features/apiSlice";
+
+export interface SearchSuggestionsProps {
+  suggestions: AutocompleteSearchSuggestion[];
+  onSelect: (text: string) => void;
+  loading?: boolean;
+}
+
+const suggestionIconMap = {
+  hut: CabinOutlined,
+  peak: PeakIcon,
+  range: LandscapeOutlined,
+  term: SearchOutlined,
+};
+
+export default function SearchSuggestions({
+  suggestions,
+  onSelect,
+}: SearchSuggestionsProps) {
+  return (
+    <List>
+      {suggestions.map(({ text, type }) => {
+        const Icon = suggestionIconMap[type] ?? null;
+        return (
+          <ListItem disablePadding key={`${type}${text}`}>
+            <ListItemButton
+              onClick={(ev) => {
+                onSelect(text);
+                ev.preventDefault();
+              }}
+            >
+              <ListItemIcon>
+                <div
+                  style={{
+                    borderRadius: "10px",
+                    backgroundColor: "#d9d9d9",
+                    height: "40px",
+                    width: "40px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  {Icon ? (
+                    <Icon style={{ fontSize: "24px", color: "#8b8b8b" }} />
+                  ) : null}
+                </div>
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+}
