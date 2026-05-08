8848e0d83e9c2c662ecfd7c01c816d243e7dcd3a
martinheppner <martinheppner@users.noreply.github.com>
2026-05-08 21:22:31 +0200

    fix: remove leaked production credentials from diana-config.js

--- apps/backend/src/diana-config.js
+++ apps/backend/src/diana-config.js
@@ -1,12 +1,5 @@
-export const CLIENT_ID = "***REMOVED***";
+// These credentials have a very, very limited contingent. If you need to test more, please contact office@zuugle-service.com
+export const CLIENT_ID = "JQHV1ZuDMS7nVONfcgCFeob8L16wpUPcIw88vIMm";
 export const CLIENT_SECRET =
-    "***REMOVED***";
+    "dkElmAIo2TefZmBKcTFiNuFXOcxqtatwWucE6DNYIdDGOaU5RkgTjdyo8Ahzy9GSWmYDbP9jml4vtgA6QIH61fE8qwjotQCzBh6Ko3rmkCznPu9KnmsZoyB4pTEmRIFZ";
 export const dev_mode = false;
-
-// zuugle.at Credeintaisl
-// client ID: ***REMOVED***
-// secret: ***REMOVED***
-
-// zuugle.at dev
-// ID: JQHV1ZuDMS7nVONfcgCFeob8L16wpUPcIw88vIMm
-// secret: dkElmAIo2TefZmBKcTFiNuFXOcxqtatwWucE6DNYIdDGOaU5RkgTjdyo8Ahzy9GSWmYDbP9jml4vtgA6QIH61fE8qwjotQCzBh6Ko3rmkCznPu9KnmsZoyB4pTEmRIFZ
