49c505804c505d7abba541cdf5957aa409c87555
martinheppner <martinheppner@users.noreply.github.com>
2026-06-13 12:34:01 +0200

    Convert diana-config to CommonJS module format

--- apps/backend/src/diana-config.js
+++ apps/backend/src/diana-config.js
@@ -1,4 +1,6 @@
 // These credentials have a very, very limited contingent. If you need to test more, please contact office@zuugle-service.com
-export const CLIENT_ID = "RXRVYzE6oijNslyvyyp78H4gbDAOXkG2A80npXXb";
-export const CLIENT_SECRET =
+const CLIENT_ID = "RXRVYzE6oijNslyvyyp78H4gbDAOXkG2A80npXXb";
+const CLIENT_SECRET =
     "30OThx1KVA3GoiiRy262jD4acDL0DIMxkTHnRNh8uOrrijqoJg2tnlG199yYxYteks8rkzDlffoQ4CYgiFomvXEBlL2WewKph3dNYmo9z7SnGmyGInbnJCeul5xwguQc";
+
+module.exports = { CLIENT_ID, CLIENT_SECRET };
